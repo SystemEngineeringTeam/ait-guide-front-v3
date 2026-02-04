4eb665a32dacb849f9bc97143e23ac512c6302b1
SatooRu65536 <ryuto65536@gmail.com>
2026-02-04 09:52:41 +0900

    add: 10号館

--- src/geojson/index.ts
+++ src/geojson/index.ts
@@ -5,9 +5,14 @@ export interface GeoJSONData {
   data: FeatureCollection;
 }
 
+import b10 from './buildings/10.json';
 import b14 from './buildings/14.json';
 
 export const GEO_JSON_DATA: GeoJSONData[] = [
+  {
+    name: '10号館',
+    data: b10 as FeatureCollection,
+  },
   {
     name: '14号館',
     data: b14 as FeatureCollection,
