6cb2bf21afa4fe309002fa2f29ce2d780b983be4
SatooRu65536 <ryuto65536@gmail.com>
2026-02-04 11:45:26 +0900

    feat: Page コンポーネントに dynamic プロパティを追加

--- src/app/geojson/page.tsx
+++ src/app/geojson/page.tsx
@@ -3,3 +3,5 @@ import GeoJsonBuildPage from '@/page/GeoJsonBuildPage';
 export default function Page() {
   return <GeoJsonBuildPage />;
 }
+
+export const dynamic = 'force-static';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,3 +3,5 @@ import MapPage from '@/page/MapPage';
 export default function Page() {
   return <MapPage />;
 }
+
+export const dynamic = 'force-static';
