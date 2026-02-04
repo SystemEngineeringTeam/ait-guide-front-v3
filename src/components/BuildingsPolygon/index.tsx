f32090b448cfb176d276448f6b22acec135fe504
SatooRu65536 <ryuto65536@gmail.com>
2026-02-04 11:52:15 +0900

    feat: 建物名を追加

--- src/components/BuildingsPolygon/index.tsx
+++ src/components/BuildingsPolygon/index.tsx
@@ -15,14 +15,26 @@ export default function BuildingPolygons({ data }: Props) {
       {data.map((building, i) => {
         const color = getFeaturesColor(building) ?? DEFAULT_COLOR;
         const darkerColor = darkenColor(color, 0.1);
+        
+        // 最初のフィーチャーに建物名を追加
+        const modifiedData = {
+          ...building.data,
+          features: building.data.features.map((f, idx) => ({
+            ...f,
+            properties: {
+              ...f.properties,
+              _buildingName: idx === 0 ? building.name : undefined,
+            },
+          })),
+        };
 
         return (
-          <Source key={i} type="geojson" data={building.data}>
+          <Source key={i} type="geojson" data={modifiedData}>
             <Layer
               type="fill"
               paint={{
                 'fill-color': color,
-                'fill-opacity': 0.3,
+                'fill-opacity': 0.5,
               }}
             />
             <Layer
@@ -32,6 +44,23 @@ export default function BuildingPolygons({ data }: Props) {
                 'line-width': 2,
               }}
             />
+            {building.name && (
+              <Layer
+                type="symbol"
+                layout={{
+                  'text-field': ['get', '_buildingName'],
+                  'text-size': 14,
+                  'text-offset': [0, 0],
+                  'text-anchor': 'center',
+                }}
+                paint={{
+                  'text-color': '#000000',
+                  'text-halo-color': '#ffffff',
+                  'text-halo-width': 2,
+                }}
+                filter={['!=', ['get', '_buildingName'], null]}
+              />
+            )}
           </Source>
         );
       })}
