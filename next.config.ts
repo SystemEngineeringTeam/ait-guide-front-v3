b8f22355943d489fea7876cb25a340d3d578e447
SatooRu65536 <ryuto65536@gmail.com>
2026-02-04 11:45:22 +0900

    fix: next.config.ts から出力設定を削除

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
   pageExtensions: ['tsx', 'mdx'],
   sassOptions: {
     additionalData: '@use "@/styles/modules" as *;',
