84581d6298d784cbaa93d1b06983459e59411672
SatooRu65536 <ryuto65536@gmail.com>
2026-02-04 12:20:17 +0900

    feat: BottomSheet

---
+++ src/components/BottomSheet/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Sheet } from 'react-modal-sheet';
+import { useState } from 'react';
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+export default function BottomSheet({ children }: Props) {
+  const [isOpen, setOpen] = useState(true);
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+
+      <Sheet isOpen={isOpen} onClose={() => setOpen(false)} snapPoints={[0, 0.1, 0.2, 1]} initialSnap={2}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    </>
+  );
+}
--- src/page/MapPage/index.tsx
+++ src/page/MapPage/index.tsx
@@ -2,11 +2,15 @@ import PageLayout from '@/layout/PageLayout';
 import Map from '@/components/Map';
 import BuildingPolygons from '@/components/BuildingsPolygon';
 import { GEO_JSON_DATA } from '@/geojson';
+import BottomSheet from '@/components/BottomSheet';
 
 export default function MapPage() {
   return (
     <PageLayout>
       <Map>
+        <BottomSheet>
+          <p>hello</p>
+        </BottomSheet>
         <BuildingPolygons data={GEO_JSON_DATA} />
       </Map>
     </PageLayout>
