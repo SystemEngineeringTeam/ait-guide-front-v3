647bb6d2127bf24264adaa41d2f337ee4f0ad088
SatooRu65536 <ryuto65536@gmail.com>
2026-02-08 03:37:55 +0900

    feat: rewrite の設定を追加

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,20 @@ import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
 
 initOpenNextCloudflareForDev();
 
+const API_URL = process.env.API_URL;
+if (!API_URL) throw new Error('API_URL is not defined in environment variables');
+
 const nextConfig: NextConfig = {
   pageExtensions: ['tsx', 'mdx'],
   sassOptions: {
     additionalData: '@use "@/styles/modules" as *;',
   },
+  rewrites: async () => [
+    {
+      source: '/api/:path*',
+      destination: `${API_URL}/api/:path*`,
+    },
+  ],
 };
 const withMDX = createMDX({});
 
--- src/hooks/useRoute.ts
+++ src/hooks/useRoute.ts
@@ -6,26 +6,17 @@ import { toValidCoordinate } from '@/utils/convert';
 import { ofetch } from 'ofetch';
 
 interface RouteResponse {
-  route: { lat: number; lng: number }[];
+  route: { lat: number; lng: number }[] | null;
 }
 
 async function fetchRoute(location: Coord, destinationId: string): Promise<Coord[]> {
   if (!location) return [];
 
-  const apiHost = process.env.NEXT_PUBLIC_API_URL;
-
-  if (apiHost === undefined) {
-    throw new Error('process.env.NEXT_PUBLIC_API_URL is undefined');
-  }
-
-  const url = new URL('/api/get/route', apiHost);
-  url.searchParams.append('lat', location[1].toString());
-  url.searchParams.append('lng', location[0].toString());
-  url.searchParams.append('end', destinationId);
-
   try {
-    const data = await ofetch<RouteResponse>(url.toString());
-    return data.route.map((p) => [p.lng, p.lat]);
+    const data = await ofetch<RouteResponse>(
+      `/api/get/route?lat=${location[1]}&lng=${location[0]}&end=${destinationId}`,
+    );
+    return data.route?.map((p) => [p.lng, p.lat]) ?? [];
   } catch (e) {
     console.error('Failed to fetch route:', e);
     return [];
