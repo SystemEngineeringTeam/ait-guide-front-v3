e10848664b7ebf415e52d648c3f4f9508887eebd
SatooRu65536 <ryuto65536@gmail.com>
2026-02-09 05:56:49 +0900

    feat: 1階部分の色

--- src/components/FacilityHighlight/index.tsx
+++ src/components/FacilityHighlight/index.tsx
@@ -4,6 +4,7 @@ import { DEFAULT_COLOR } from '@/consts/colors';
 import { darkenColor, getFeaturesColor } from '@/utils/color';
 import { useMemo } from 'react';
 import { FACILITY_POLYGON_LINE_LAYER_ID } from '@/consts/layerId';
+import type { FeatureCollection, Feature, MultiPolygon } from 'geojson';
 
 interface Props {
   id: string;
@@ -16,10 +17,46 @@ export default function FacilityHighlight({ id, outline = false, fill = false }:
 
   if (!facility) return null;
 
-  const darkerColor = useMemo(() => darkenColor(getFeaturesColor(facility) ?? DEFAULT_COLOR, 0.1), [facility]);
+  const color = getFeaturesColor(facility) ?? DEFAULT_COLOR;
+  const darkerColor = darkenColor(color, 0.1);
+
+  const highlightData = useMemo(() => {
+    const firstFeature = facility.data.features.at(0);
+
+    if (firstFeature?.geometry.type !== 'MultiPolygon') {
+      return {
+        type: 'FeatureCollection',
+        features: facility.data.features.map((f) => ({
+          ...f,
+          properties: { ...f.properties, dashed: false },
+        })),
+      } satisfies FeatureCollection;
+    }
+
+    const polygonDivision = facility.polygonDivision ?? 1;
+    const coordsList = firstFeature.geometry.coordinates;
+
+    return {
+      type: 'FeatureCollection',
+      features: coordsList.slice(0, polygonDivision).map(
+        (coord, i) =>
+          ({
+            type: 'Feature',
+            properties: {
+              ...firstFeature.properties,
+              dashed: false,
+            },
+            geometry: {
+              type: 'MultiPolygon',
+              coordinates: [coord],
+            },
+          }) satisfies Feature<MultiPolygon>,
+      ),
+    } satisfies FeatureCollection;
+  }, [facility]);
 
   return (
-    <Source type="geojson" data={facility.data}>
+    <Source type="geojson" data={highlightData}>
       {fill && (
         <Layer
           type="fill"
--- src/components/FacilityPolygons/index.tsx
+++ src/components/FacilityPolygons/index.tsx
@@ -4,53 +4,81 @@ import { GeoJSONData } from '@/types/facilities';
 import { Layer, Source } from 'react-map-gl/maplibre';
 import { darkenColor, getFeaturesColor } from '@/utils/color';
 import { DEFAULT_COLOR } from '@/consts/colors';
-import type { FeatureCollection } from 'geojson';
+import type { FeatureCollection, MultiPolygon, Feature } from 'geojson';
 import { useMemo } from 'react';
 import { FACILITY_POLYGON_FILL_LAYER_ID, FACILITY_POLYGON_LINE_LAYER_ID } from '@/consts/layerId';
+import { FacilityId } from '@/consts/facilityId';
 
 interface Props {
   facilities: GeoJSONData[];
 }
 
 export default function FacilityPolygons({ facilities }: Props) {
-  const sorted = [...facilities].sort((a, b) => {
-    if (a.type !== 'passage' && b.type === 'passage') return 1;
-    if (a.type === 'passage' && b.type !== 'passage') return -1;
-    return 0;
-  });
-
   const data = useMemo(
     () =>
       ({
         type: 'FeatureCollection',
-        features: sorted.flatMap((facility) => {
-          const color = getFeaturesColor(facility) ?? DEFAULT_COLOR;
-          const darkerColor = darkenColor(color, 0.1);
+        features: facilities
+          .flatMap((facility) => {
+            const color = getFeaturesColor(facility) ?? DEFAULT_COLOR;
+            const lineColor = darkenColor(color, 0.1);
+            const firstFloorColor = darkenColor(color, 0.05);
 
-          return facility.data.features.map((f) => ({
-            ...f,
-            properties: {
-              ...f.properties,
-              facilityId: facility.id,
-              fill: color,
-              line: darkerColor,
-            },
-          }));
-        }),
+            const firstFeature = facility.data.features.at(0);
+            const isPassage = facility.type === 'passage';
+            if (isPassage || firstFeature?.geometry.type !== 'MultiPolygon') {
+              return [
+                facility.data.features.map(
+                  (f) =>
+                    ({
+                      ...f,
+                      properties: {
+                        ...f.properties,
+                        facilityId: facility.id,
+                        fill: color,
+                        line: lineColor,
+                        dashed: false,
+                      },
+                    }) satisfies Feature,
+                ),
+              ];
+            }
+
+            const polygonDivision = facility.polygonDivision ?? 1;
+            const coordsList = firstFeature.geometry.coordinates;
+            return coordsList.map(
+              (coord, i) =>
+                ({
+                  type: 'Feature',
+                  properties: {
+                    ...firstFeature.properties,
+                    facilityId: facility.id,
+                    fill: i >= polygonDivision ? firstFloorColor : color,
+                    line: lineColor,
+                    dashed: i >= polygonDivision,
+                  },
+                  geometry: {
+                    type: 'MultiPolygon',
+                    coordinates: [coord],
+                  },
+                }) satisfies Feature<MultiPolygon>,
+            );
+          })
+          .flat(),
       }) satisfies FeatureCollection,
-    [sorted],
+    [facilities],
   );
 
   return (
     <Source type="geojson" data={data}>
       <Layer
         id={FACILITY_POLYGON_FILL_LAYER_ID}
-        type="fill-extrusion"
+        type="fill"
         paint={{
-          'fill-extrusion-color': ['get', 'fill'],
-          'fill-extrusion-height': ['coalesce', ['get', 'height'], 0],
-          'fill-extrusion-base': 0,
-          'fill-extrusion-opacity': 0.8,
+          'fill-color': ['get', 'fill'],
+          'fill-opacity': 0.8,
+          // 'fill-extrusion-height': ['coalesce', ['get', 'height'], 0],
+          // 'fill-extrusion-base': 0,
         }}
       />
       <Layer
@@ -59,6 +87,12 @@ export default function FacilityPolygons({ facilities }: Props) {
         paint={{
           'line-color': ['get', 'line'],
           'line-width': 2,
+          'line-dasharray': [
+            'case',
+            ['get', 'dashed'],
+            ['literal', [2, 2]], // 破線
+            ['literal', [1, 0]], // 実線
+          ],
         }}
       />
     </Source>
