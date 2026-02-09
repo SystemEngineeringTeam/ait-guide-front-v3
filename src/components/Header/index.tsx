ee6031bb3a088e656fb59af3a7a6716890ae90ee
SatooRu65536 <ryuto65536@gmail.com>
2026-02-09 14:35:23 +0900

    feat: ヘッダーにリンクを追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link';
 import styles from './index.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>AIT ガイド</h1>
-      <p className={styles.createdBy}>created by シス研</p>
+      <Link href="/" className={styles.link}>
+        <h1 className={styles.title}>AIT ガイド</h1>
+        <p className={styles.createdBy}>created by シス研</p>
+      </Link>
     </header>
   );
 }
