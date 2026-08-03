8b5628ef121b5d3727a3311c0b3999557b3fccff
SatooRu65536 <ryuto65536@gmail.com>
2026-08-03 21:41:56 +0900

    feat: ノード削除機能

---
+++ src/hooks/useRouteBuilder/components/RouteBuilderPanel/EdgeRemove/index.tsx
@@ -0,0 +1,19 @@
+import PanelButton from '@/components/PanelButton';
+import styles from './index.module.scss';
+import { useSelectedEdgeValue } from '@/hooks/useRouteBuilder/hooks/useSelectedTarget';
+import { useEdgesSetter } from '@/hooks/useRouteBuilder/hooks/useEdges';
+
+export default function EdgeRemove() {
+  const selectedEdge = useSelectedEdgeValue();
+  const { removeEdge } = useEdgesSetter();
+
+  if (!selectedEdge) return null;
+
+  return (
+    <div className={styles.container}>
+      <PanelButton emoji="🗑️" selected onClick={() => removeEdge(selectedEdge.uuid)} danger>
+        削除
+      </PanelButton>
+    </div>
+  );
+}
